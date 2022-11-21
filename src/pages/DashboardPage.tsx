49e9e5ffdd22015207b59a1096758aaf4539d7a3
Heijden,Rafaël R.A.M. van der <rafael.vanderheijden@student.fontys.nl>
2022-11-21 16:37:01 +0100

    fix: page height adjusted to pixel size

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const DashboardPage = (props: Props) => {
   return (
-    <div className="w-full text-[32px] bg-white h-[90rem] opacity-95 backdrop-filter backdrop-blur-[8px]">
+    <div className="w-full text-[32px] bg-white h-[85rem] opacity-95 backdrop-filter backdrop-blur-[8px]">
       Dashboard
     </div>
   );
