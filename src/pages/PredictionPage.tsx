1b7809d96abbec992cc64aa6d1b00832627826dd
Luc Swinkels <luc.swinkels@live.nl>
2022-11-23 10:24:13 +0100

    chore: second dev test for tablet

--- src/pages/PredictionPage.tsx
+++ src/pages/PredictionPage.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const PredictionPage = (props: Props) => {
   return (
     <div className="w-full text-[32px] bg-white h-[85rem] opacity-95 backdrop-blur-sm">
-      Predictions
+      PredictionsTESTTEST
     </div>
   );
 };
