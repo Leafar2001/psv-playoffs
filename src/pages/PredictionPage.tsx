de7f89c9d68debc1095811d1b26f6829c3e65e98
Luc Swinkels <luc.swinkels@live.nl>
2022-11-23 11:09:35 +0100

    refactor: fixed height for container and replaced it with min height

--- src/pages/PredictionPage.tsx
+++ src/pages/PredictionPage.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const PredictionPage = (props: Props) => {
   return (
-    <div className="w-full text-[32px] bg-white h-[70vh] opacity-95 p-8">
+    <div className="w-full text-[32px] bg-white min-h-[70vh] opacity-95 p-8">
       Predictions
     </div>
   );
