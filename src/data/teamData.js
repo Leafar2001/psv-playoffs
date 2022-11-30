01f3ad5931fda30ba43a28b6b4fe80fd45f5e09a
Leafar2001 <raafie.vdheijden@gmail.com>
2022-11-30 15:37:39 +0100

    working piechart with dummy data

---
+++ src/data/teamData.js
@@ -0,0 +1,110 @@
+
+export const teamData = [
+    {
+        id: 1, 
+        teamStats: 
+        [
+            {
+                teamName: "PSV",
+                generalStats: 
+                [
+                    {
+                        wins: 1,
+                        losses: 1,
+                        draws: 1,
+                        headToHead: 1,
+                        averagePossesion: "60%", 
+                        totGoalsScored: 24,
+                        averageGoals: 2,
+                        totGoalsAgainst: 15,
+                        averageGoalsAgainst: 1
+                    }
+                ],
+                players: 
+                [
+                    {
+                        playerId: 1,
+                        playerName: "Cody Gakpo",
+                        appearances: 24,
+                        minutesPlayed: 1960,
+                        starts: 24,
+                        subOns: 0,
+                        injuryStatus: false,
+                        goals: 13,
+                        assists:17,
+                        yellowCards: 1,
+                        redCards: 0
+                    },
+                    {
+                        playerId: 2,
+                        playerName: "Xavi Simons",
+                        appearances: 22,
+                        minutesPlayed: 1567,
+                        starts: 19,
+                        subOns: 3,
+                        injuryStatus: false,
+                        goals: 10,
+                        assists: 4,
+                        yellowCards: 3,
+                        redCards: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 2, 
+        teamStats: 
+        [
+            {
+                teamName: "AJAX",
+                generalStats: 
+                [
+                    {
+                        wins: 1,
+                        losses: 1,
+                        draws: 1,
+                        headToHead: 1,
+                        averagePossesion: "60%", 
+                        totGoalsScored: 24,
+                        averageGoals: 2,
+                        totGoalsAgainst: 15,
+                        averageGoalsAgainst: 1
+                    }
+                ],
+                players: 
+                [
+                    {
+                        playerId: 3,
+                        playerName: "Steven Bergwijn",
+                        appearances: 21,
+                        minutesPlayed: 1687,
+                        starts: 20,
+                        subOns: 1,
+                        injuryStatus: false,
+                        goals: 11,
+                        assists:3,
+                        yellowCards: 1,
+                        redCards: 0
+                    },
+                    {
+                        playerId: 4,
+                        playerName: "Mohammed Kudus",
+                        appearances: 21,
+                        minutesPlayed: 979,
+                        starts: 10,
+                        subOns: 11,
+                        injuryStatus: false,
+                        goals: 10,
+                        assists: 2,
+                        yellowCards: 1,
+                        redCards: 0
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+
+
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import HeaderBar from "../components/HeaderBar";
 
+import PieChart from "../components/charts/PieChart";
+
 type Props = {};
 
 const DashboardPage = (props: Props) => {
@@ -10,6 +12,9 @@ const DashboardPage = (props: Props) => {
       <NavBar/>
       <HeaderBar Heading={"Dashboard"} />
       <div className="w-full text-[32px] bg-white min-h-[70vh] opacity-95 p-8">
+        <div className="">
+          <PieChart />
+        </div>
       </div>
     </div>
   );
