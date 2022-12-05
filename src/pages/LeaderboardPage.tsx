d6703e1076daa1eba9cd2e76bb4b87b7eeb2ed69
Swinkels, Luc L <l.swinkels-ad@student.fontys.nl>
2022-12-05 12:34:52 +0100

    feat: add player prediction to prediction form

--- src/pages/LeaderboardPage.tsx
+++ src/pages/LeaderboardPage.tsx
@@ -2,10 +2,27 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import HeaderBar from "../components/HeaderBar";
 import Leaderboard from "../components/Leaderboard";
+import { teamData } from "../data/teamData";
 
 type Props = {};
 
 const LeaderboardPage = (props: Props) => {
+  const scoringSystem = {
+    goal: 10,
+    assist: 5,
+  };
+
+  const currentMatchData = [
+    // scoredBy & assistedBy checks playerId
+    {
+      goals: [
+        { timeScored: 17, scoredBy: 1, assistedBy: 2 },
+        { timeScored: 37, scoredBy: 2 },
+        { timeScored: 47, scoredBy: 2, assistedBy: 4 },
+      ],
+    },
+  ];
+
   return (
     <div>
       <NavBar />
