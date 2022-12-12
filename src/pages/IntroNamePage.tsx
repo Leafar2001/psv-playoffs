126ef9698ddf7d0407e1c9900eb7f5b0fff85787
Leafar2001 <raafie.vdheijden@gmail.com>
2022-12-12 14:02:51 +0100

    Name is required now

--- src/pages/IntroNamePage.tsx
+++ src/pages/IntroNamePage.tsx
@@ -3,14 +3,24 @@ import { Link } from "react-router-dom";
 import HeaderBar from "../components/HeaderBar"
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { FiCircle } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+
+import { redirect } from "react-router-dom";
 
 type Props = {};
 
 const IntroPage = (props: Props) => {
 
+  let navigate = useNavigate();
+
   const handleNameInput = (e: React.FormEvent<HTMLInputElement>) => {
     sessionStorage.setItem("name", e.currentTarget.value);
   };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate("/dashboard")
+  }
 
   return (
     <div>
@@ -22,18 +32,26 @@ const IntroPage = (props: Props) => {
       <div className="h-36"></div>
       <div className="flex justify-center text-black relative z-10 pb-2 h-12">What is your name?</div>
       <div className="flex justify-center">
-        <input
-          type="text"
-          onChange={handleNameInput}
-          className="bg-[#D9D9D9] p-1 pl-4 rounded w-96 h-14"
-          required
-          placeholder="First name"
-        />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={handleNameInput}
+            className="bg-[#D9D9D9] p-1 pl-4 rounded w-96 h-14"
+            required
+            placeholder="First name"
+          />
+          <div className="h-20"></div>
+          <div className="flex relative justify-center z-10">
+            <input 
+              type="submit"
+              value="Get started"
+              className="bg-red text-white text-xxl flex px-4 py-2 font-bold rounded cursor-pointer"
+              required
+            />
+          </div>
+        </form>
       </div>
-      <div className="h-60"></div>
-      <div className="flex relative justify-end z-10">
-        <Link to="/dashboard" className="font-bold text-3xl" style={{display: "flex", alignItems: "center", color: "#EA0000"}}>Go to dashboard<FiChevronRight className="text-3xl mx-2"/></Link>
-      </div>
+      
     </div>
 
   );
